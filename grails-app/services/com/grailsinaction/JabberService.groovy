0a088d0a000a5d1c4a5075132a611900e85f0d0d
bramseyerktz <bramseyer@kaitzen-solutions.com>
2016-02-11 14:53:55 -0300

    Add test for sendMessage service

--- grails-app/services/com/grailsinaction/JabberService.groovy
+++ grails-app/services/com/grailsinaction/JabberService.groovy
@@ -1,7 +1,23 @@
 package com.grailsinaction
 
 class JabberService {
+    static expose = ['jms']
+    static destination = "jabberInQ"
+    static sendQueue = "jabberOutQ"
+
     def jmsService
+
+    void onMessage(msq){
+        log.debug "Got incoming jabber response from: ${msg.jabberId}"
+        try {
+            def profile = Profile.findByJabberAddress(msg.jabberId)
+            if (profile){
+                profile.user.addToPosts(new Post(content: msj.content))
+            }
+        } catch (t){
+            log.error("Error adding post for ${msg.jabberId}", t)
+        }
+    }
 
     void sendMessage(post, jabberIds) {
         log.debug "Sending jabber message for ${post.user.userId}..."
--- test/integration/com/grailsinaction/JabberServiceIntegrationSpec.groovy
+++ test/integration/com/grailsinaction/JabberServiceIntegrationSpec.groovy
@@ -5,6 +5,11 @@ import grails.test.spock.IntegrationSpec
 
 class JabberServiceIntegrationSpec extends IntegrationSpec {
     def jabberService
+    def jmsService
+
+    def jmsOutputQueue = "jabberOutQ"
+
+    static transactional = false
 
     def "First send to a queue"() {
         given: "Some sample queue data"
@@ -14,5 +19,20 @@ class JabberServiceIntegrationSpec extends IntegrationSpec {
                          "peter@grailsinaction.com"]
         expect:
         jabberService.sendMessage(post, jabberIds)
+    }
+
+    def "Send message to the jabber queue"(){
+        given: "some sample queue data"
+        def post = [user: [userId: 'chuck_norris'],
+                    content: 'is backstroking across the atlantic']
+        def jabberIds = ["glen@grailsinaction.com",
+                        "peter@grailsinaction.com"]
+        def msgListBeforeSend = jmsService.browse(jabberService.sendQueue)
+
+        when:
+        jabberService.sendMessage(post, jabberIds)
+
+        then:
+        jmsService.browse(jabberService.sendQueue).size() == msgListBeforeSend.size() + 1
     }
 }
