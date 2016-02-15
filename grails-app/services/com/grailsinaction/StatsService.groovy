edb734648bee6d116e4163a45ab4e9b8c056eb9a
bramseyerktz <bramseyer@kaitzen-solutions.com>
2016-02-15 10:37:10 -0300

    Finish work with redis

--- grails-app/services/com/grailsinaction/StatsService.groovy
+++ grails-app/services/com/grailsinaction/StatsService.groovy
@@ -13,9 +13,23 @@ class StatsService {
         String dateToday = new Date().format("yy-MM-dd")
         String redisTotalsKey = "daily.stat.totalPosts.${dateToday}"
 
-        log.debug "New Post from: ${newPost.user.loginId}"
+        //log.debug "New Post from: ${newPost.user.loginId}"
         redisService.incr(redisTotalsKey)
 
+        String redisTotalsByUserKey = "daily.stat.totalsByUser.${dateToday}"
+        redisService.zincrby(redisTotalsByUserKey, 1, newPost.user.loginId)
+        int usersPostsToday = redisService.zscore(redisTotalsByUserKey, newPost.user.loginId)
+
+        log.debug("Incremented daily stat for ${newPost.user.loginId} to ${usersPostsToday}")
+
         log.debug "Total Posts at: ${redisService.get(redisTotalsKey)}"
+    }
+
+    def getTodaysTopPosters(){
+        String dateToday = new Date().format("yy-MM-dd")
+        String redisTotalsByUserKey = "daily.stat.totalsByUser.${dateToday}"
+        def tuples = redisService.zrevrangeWithScores(redisTotalsByUserKey, 0, 1000)
+            tuples.each {tuple -> log.debug("Posts for ${tuple.element} -> ${tuple.score}")}
+        return tuples
     }
 }
